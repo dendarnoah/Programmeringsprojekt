7d78b73809d3766ea7ad13d9ae338ed1a1655bb8
dendarnoah <116350963+dendarnoah@users.noreply.github.com>
2023-01-20 11:52:59 +0100

    Update Del 1 objektorientering.py

--- Del 1 objektorientering.py
+++ Del 1 objektorientering.py
@@ -6,7 +6,7 @@ strength_bonus = 0
 hp = 5 + 2*level
 attack = 2 + 1*strength_bonus
 
-
+#skapar klassen player och deklarerar egenskaper.
 class Player():
     def __init__(self, name):
         self.name = name
@@ -15,6 +15,7 @@ class Player():
         self.exp = exp
         self.attack = attack
 
+#skapar funktionen level up som kollar på specifika krav på exp ökar variabeln level samt printar spelarens level och uppdaterar hp och attack.
 def level_up():
     global level, exp, strength_bonus
     while exp >= 20 + 0.2 * level:
@@ -28,6 +29,7 @@ def level_up():
         attack = 2 + 1 * strength_bonus
         player.attack = int(attack)
 
+#skapar klassen monster samt deklarerar egenskaper på de olika typerna av monster.
 class Monster():
     def __init__(self, monstertype):
         self.type = monstertype
@@ -61,7 +63,7 @@ class Monster():
     def get_level(self):
         print(level)
 
-
+#deklarerar klassen chest och sedan skapar de två kisttyperna och även ger variabeln item ett värde som väljer vilket item som droppas.
 class Chest():
     def __init__(self, chesttype):
         self.type = chesttype
@@ -69,39 +71,39 @@ class Chest():
         if chesttype == 1:
             random_item = random.randint(1, 100)
             if random_item == 1:
-                item = 6
-            elif random_item == 2:
-                item = 5
-            elif 3 <= random_item <= 17:
-                item = 4
-            elif 18 <= random_item <= 32:
-                item = 3
-            elif 33 <= random_item <= 64:
-                item = 2
-            elif 65 <= random_item <= 100:
                 item = 1
+            elif random_item == 2:
+                item = 2
+            elif 3 <= random_item <= 17:
+                item = 3
+            elif 18 <= random_item <= 32:
+                item = 4
+            elif 33 <= random_item <= 64:
+                item = 5
+            elif 65 <= random_item <= 100:
+                item = 6
             self.item = item
             self.expdrop = 2
 
         elif chesttype == 2:
             random_item = random.randint(1, 100)
             if random_item == 1:
-                item = 1
-            elif random_item == 2:
-                item = 2
-            elif 3 <= random_item <= 17:
-                item = 3
-            elif 18 <= random_item <= 32:
-                item = 4
-            elif 33 <= random_item <=64:
-                item = 5
-            elif 65 <= random_item <= 100:
                 item = 6
+            elif random_item == 2:
+                item = 5
+            elif 3 <= random_item <= 17:
+                item = 4
+            elif 18 <= random_item <= 32:
+                item = 3
+            elif 33 <= random_item <=64:
+                item = 2
+            elif 65 <= random_item <= 100:
+                item = 1
             self.item = item
             self.expdrop = 5
 
 
-
+#skapar klassen item som innehåller alla olika items och deras strengthmod.
 class Item():
     def __init__(self, itemtype):
         self.type = itemtype
@@ -127,9 +129,11 @@ class Item():
     def __str__(self):
         return f"{self.name} (strength modifier: {self.strengthmod})"
 
-
+#skapar inventory
 inventory = []
 
+#skapar funktionen add_item till inventory där den kollar ifall spelarens inventory inte är fullt och även uppdaterar spelarens attackskada.
+#Ifall väskan är full kollar den ifall spelaren vill byta ut ett föremål och även vilket, spelaren kan även ångra och gå tillbaks.
 def add_item(item):
     global strength_bonus
     if len(inventory) < 5:
@@ -167,18 +171,18 @@ def add_item(item):
             else:
                 print("Ogiltigt svar, försök igen.")
 
-
+#skapar funktionen show_inventory där spelaren ser innehållet i väskan.
 def show_inventory():
     for i, item in enumerate(inventory):
         print(f"{i+1}. {item}")
 
-
+#Printar några bokstäver i taget.
 sentence = "Du är en vilsen äventyrare som kommit långt hemifrån."
 for char in sentence:
     print(char, end='', flush=True)
     time.sleep(0.02)
 
-
+#Spelaren kan välja namn och det dubbelkollas ifall spelaren vill ha namnet.
 Karaktär = input("Välj ditt namn ->")
 confirmation = input(f"Är du säker på att du vill välja {Karaktär} som ditt namn? (ja/nej)")
 while confirmation.lower() != "ja":
@@ -193,6 +197,7 @@ styrka = 0
 försvar = 0
 print("Du har 10 poäng att spendera")
 
+#Spelaren får välja styrka och försvar, den kollar ifall spelaren inte skriver fel då görs det om. Den dubbelkollar även så att spelaren inte väljer ett för högt tal eller negativt tal.
 while poäng > 0:
     while True:
         try:
@@ -219,18 +224,22 @@ while poäng > 0:
     poäng = poäng - försvarspoäng
     print("Styrka", styrka, "försvar", försvar, "poäng", poäng)
 
+#spelarens hp deklareras och det sätts på player.
 hp = 5 + 2*level + 0.5 * försvar
-
+player.health = hp
 player = Player(Karaktär)
 
+#fortsatt utskrivt, samma som sist.
 sentence = "Du vandrar planlöst genom skogar och ängar i vad som känns som år. \nDu kommer tillslut fram till ett samhälle.\nDu upptäcker ganska omgående att allt inte är som det ska. \nDetta samhälle är helt tomt och det ända som återfinns av en civilisation är lik. \nI skogen intill ser du rörelse, du går fram för att undersöka.\nSå fort du når fram ser du att du kan välja mellan tre stigar, vilken väljer du?"
 for char in sentence:
     print(char, end='', flush=True)
     time.sleep(0.02)
 
 rumsnummer = 0
+#huvudloopen till spelet sm avbryts när spelare når nivå 10.
 while level < 10:
 
+    #gör så att första valet görs endast stig efter det dyker de tre valen upp innan stigvalet.
     while rumsnummer >= 1:
         choice = input("Du kan välja mellan att visa:\n1. inventory\n 2. spelare\n 3. fortsätt\n")
         if choice == "1" or choice.lower() == "inventory":
@@ -243,6 +252,7 @@ while level < 10:
         else:
             print("Du valde ett ogiltigt alternativ, välj igen.")
 
+#den kollar ifall spelaren inte valt en bokstav eller felaktigt svar, då tar spelaren skada.
     print("\nVälj stig mellan: 1, 2, eller 3?\n")
     while True:
         try:
@@ -272,9 +282,10 @@ while level < 10:
                 print("Du tog en skada, försök igen")
                 door = input("Välj stig mellan: 1, 2, eller 3")
 
-
+#slumpar vad som finns i rummet.
     chans = random.randint(1,100)
 
+#Spelaren stöter på ett monster som den bestrider. När monstrets hp når 0 dör det och ifall spelarens hp når 0 är det game over. Spelaren får även exp från monstret.
     if 1 <= chans <= 33:
         monstertype = random.randint(1,3)
         monster = Monster(monstertype)
@@ -300,7 +311,7 @@ while level < 10:
         rumsnummer += 1
         level_up()
 
-
+#spelet slumpar vilken typ av kista och även vilket item, den skriver även ut vilket item och även lägger till det i inventory. Det kollas även ifall spelaren ska levla up.
     if 34 <= chans <= 66:
         random_chesttype = random.randint(1,10)
         if random_chesttype == 1:
@@ -317,6 +328,7 @@ while level < 10:
         level_up()
         rumsnummer += 1
 
+#Skapar en fälla och kollar ifall spelaren dör.
     if 67 <= chans <= 100:
         print("Du klev i en fälla och tog 1 skada!")
         player.health -= 1
@@ -327,5 +339,5 @@ while level < 10:
             exit()
         rumsnummer += 1
 
-        
+#När spelarren når nivå 10 har den vunnit och detta printas.     
 print("Grattis! Du nådde level 10 och klarade spelet! Med din hjälp återhämtar sig det som fanns kvar av byn.")
